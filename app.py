97769d7455032af270a5b69a75041c09eaf7bab8
Lucas Bezerra <lbezerra@gazetaonline.com.br>
2024-04-03 19:33:40 -0300

    [UPDATE] Job - Adding a job to prevent downtime in web app

--- app.py
+++ app.py
@@ -5,9 +5,19 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 app = create_app()
 
+def prevent_inactivity():
+  import requests
+
+  result = requests.get('https://investidor-informado.onrender.com/')
+  print(result)
+  return True
+
+
 scheduler = BackgroundScheduler()
 bg_job = scheduler.add_job(func=bg_main, args=[True], trigger='interval',
                            seconds=10800, name="Atualizando Tabela Preço Justo")
 fii_job = scheduler.add_job(func=get_fiis, args=[True], trigger='interval',
                             seconds=10800, name="Atualizando Tabela FIIs")
+delay_job = scheduler.add_job(func=prevent_inactivity, trigger='interval',
+                            seconds=780, name="Previnir inatividade da aplicação no Render.com")
 scheduler.start()
