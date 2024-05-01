f313f03f799b0b8163d0cfadae30428e0487880b
Lucas Bezerra <lbezerra@gazetaonline.com.br>
2024-04-30 23:40:14 -0300

    [UPDATE] Cleaning code

--- api/methods/fii.py
+++ api/methods/fii.py
@@ -1,6 +1,4 @@
-import os,sys 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from fundamentus_fii import fii, resultado_fii, utils
+from ..fundamentus_fii import fii, resultado_fii, utils
 import pandas as pd
 import json
 import time
